bd2a6a4c6811b7f27961ff237c2dec021abd9929
Matt B <33362871+splitpunched@users.noreply.github.com>
2018-10-10 17:49:35 -0500

    Delete orbs.js

--- js/orbs.js
+++
@@ -1,6 +0,0 @@
-var orbs = {}
-
-orbs.earthOrb = { id: 0, name: "earthOrb", label: "Earth", img: 'icons/earthOrb.png', }
-orbs.fireOrb = { id: 1, name: "fireOrb", label: "Fire", img: 'icons/fireOrb.png', }
-orbs.waterOrb = { id: 2, name: "waterOrb", label: "Water", img: 'icons/waterOrb.png', }
-orbs.airOrb = { id: 3, name: "airOrb", label: "Air", img: 'icons/airOrb.png', }
