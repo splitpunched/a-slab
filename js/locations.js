1dc315c04fa03d4c49ec4edc0dcd6d3b686edeeb
Matt B <33362871+splitpunched@users.noreply.github.com>
2018-11-05 19:35:07 -0600

    Update locations.js

--- js/locations.js
+++ js/locations.js
@@ -497,9 +497,7 @@ var locationInfo = {
 					return vm.flags.incentiveSkyPalace
                 }(),
                 accessible: function () {
-                    if (vm.flags.floorShuffle) { return true }
-                    if (!vm.itemData.chime || !vm.itemData.cube) { return false }
-                    else { return vm.itemData.airship.tracked }
+                    return { (vm.flags.floorShuffle || vm.itemData.cube && (vm.flags.entranceShuffle || (vm.itemData.chime && vm.mapAccess.onrac))) }
                 }(),
 				display: function() {
 					if (vm.flags.incentiveSkyPalace || !vm.flags.shuffleTreasures) { return true }
